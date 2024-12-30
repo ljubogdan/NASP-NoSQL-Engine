9b69e70888d4d2e3600c9417e69a5e404d706fb2
NotixOfficial <mcnprodajax@gmail.com>
2024-12-30 23:11:01 +0100

    [CHANGE] B-tree promenjen

--- internal/trees/b_tree.go
+++ internal/trees/b_tree.go
@@ -6,7 +6,8 @@ import (
 
 type BTreeNode struct {
 	isLeaf   bool
-	keys     []int
+	keys     []string
+	values   [][]byte
 	children []*BTreeNode
 	t        int
 }
@@ -14,7 +15,8 @@ type BTreeNode struct {
 func NewBTreeNode(t int, isLeaf bool) *BTreeNode {
 	return &BTreeNode{
 		isLeaf:   isLeaf,
-		keys:     make([]int, 0),
+		keys:     make([]string, 0),
+		values:   make([][]byte, 0),
 		children: make([]*BTreeNode, 0),
 		t:        t,
 	}
@@ -35,7 +37,9 @@ func (node *BTreeNode) splitChild(i int, t int) {
 	newChild := NewBTreeNode(t, child.isLeaf)
 
 	newChild.keys = append(newChild.keys, child.keys[t:]...)
+	newChild.values = append(newChild.values, child.values[t:]...)
 	child.keys = child.keys[:t-1]
+	child.values = child.values[:t-1]
 
 	if !child.isLeaf {
 		newChild.children = append(newChild.children, child.children[t:]...)
@@ -43,19 +47,22 @@ func (node *BTreeNode) splitChild(i int, t int) {
 	}
 
 	node.children = append(node.children[:i+1], append([]*BTreeNode{newChild}, node.children[i+1:]...)...)
-	node.keys = append(node.keys[:i], append([]int{child.keys[t-1]}, node.keys[i:]...)...)
+	node.keys = append(node.keys[:i], append([]string{child.keys[t-1]}, node.keys[i:]...)...)
+	node.values = append(node.values[:i], append([][]byte{child.values[t-1]}, node.values[i:]...)...)
 }
 
-// Ubacivanje ključa u nepuni čvor
-func (node *BTreeNode) insertNonFull(k int) {
+func (node *BTreeNode) insertNonFull(k string, v []byte) {
 	i := len(node.keys) - 1
 	if node.isLeaf {
-		node.keys = append(node.keys, 0)
+		node.keys = append(node.keys, "")
+		node.values = append(node.values, nil)
 		for i >= 0 && node.keys[i] > k {
 			node.keys[i+1] = node.keys[i]
+			node.values[i+1] = node.values[i]
 			i--
 		}
 		node.keys[i+1] = k
+		node.values[i+1] = v
 	} else {
 		for i >= 0 && node.keys[i] > k {
 			i--
@@ -67,15 +74,15 @@ func (node *BTreeNode) insertNonFull(k int) {
 				i++
 			}
 		}
-		node.children[i].insertNonFull(k)
+		node.children[i].insertNonFull(k, v)
 	}
 }
 
-// Ubacivanje ključa u stablo
-func (tree *BTree) Insert(k int) {
+func (tree *BTree) Insert(k string, v []byte) {
 	if tree.root == nil {
 		tree.root = NewBTreeNode(tree.t, true)
 		tree.root.keys = append(tree.root.keys, k)
+		tree.root.values = append(tree.root.values, v)
 	} else {
 		if len(tree.root.keys) == 2*tree.t-1 {
 			newRoot := NewBTreeNode(tree.t, false)
@@ -85,16 +92,19 @@ func (tree *BTree) Insert(k int) {
 			if newRoot.keys[0] < k {
 				i++
 			}
-			newRoot.children[i].insertNonFull(k)
+			newRoot.children[i].insertNonFull(k, v)
 			tree.root = newRoot
 		} else {
-			tree.root.insertNonFull(k)
+			tree.root.insertNonFull(k, v)
 		}
 	}
 }
 
 func (node *BTreeNode) Print(level int) {
-	fmt.Printf("Level %d Keys: %v\n", level, node.keys)
+	fmt.Printf("Level %d\n", level)
+	for i, key := range node.keys {
+		fmt.Printf("  Key: %s, Value: %v\n", key, node.values[i])
+	}
 	for _, child := range node.children {
 		child.Print(level + 1)
 	}
@@ -107,20 +117,17 @@ func (tree *BTree) Print() {
 }
 
 /*
-
-// testiranje;;; nisam završio ima par problema!!!
+// testiranje
 func main() {
 	btree := NewBTree(3)
-	btree.Insert(10)
-	btree.Insert(20)
-	btree.Insert(5)
-	btree.Insert(6)
-	btree.Insert(12)
-	btree.Insert(30)
-	btree.Insert(7)
-	btree.Insert(17)
+	btree.Insert("jabuka", []byte{1, 2, 3})
+	btree.Insert("banana", []byte{4, 5, 6})
+	btree.Insert("visnja", []byte{7, 8, 9})
+	btree.Insert("boronica", []byte{10, 11, 12})
+	btree.Insert("steak", []byte{13, 14, 15})
+	btree.Insert("jaja", []byte{16, 17, 18})
+	btree.Insert("burger", []byte{19, 20, 21})
 
 	btree.Print()
 }
-
 */
