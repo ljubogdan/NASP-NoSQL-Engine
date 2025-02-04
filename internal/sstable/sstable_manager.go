cfb75563b7899783297cb3b3510e5e22a9b77a89
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-02-05 00:51:57 +0100

    [CHANGE] Create index i create summary prebačeno u sstable manager iz sstable

--- internal/sstable/sstable_manager.go
+++ internal/sstable/sstable_manager.go
@@ -5,6 +5,8 @@ import (
 	"NASP-NoSQL-Engine/internal/probabilistics"
 	"NASP-NoSQL-Engine/internal/trees"
 	"NASP-NoSQL-Engine/internal/block_manager"
+	"NASP-NoSQL-Engine/internal/entry"
+	"NASP-NoSQL-Engine/internal/config"
 )
 
 const (
@@ -69,4 +71,61 @@ func (manager *SSTableManager) Get(filename string) *SSTable {    // vraća ssta
 		}
 	}
 	return nil
+}
+
+// funkcija koja kreira Index na osnovu indexTuples, vraća niz bajtova
+func (sstm *SSTableManager) CreateNONMergeIndex(indexTuples []IndexTuple, blockSize uint32) []byte {
+	index := make([]byte, 0)
+
+	// pravi offset računamo tako što pomnožimo index bloka sa veličinom bloka
+	// i dodamo poziciju u bloku
+	for _, it := range indexTuples {
+		offset := it.BlockIndex * blockSize
+		offset += it.PositionInBlock
+
+		index = append(index, []byte(it.Key)...)
+		index = append(index, 0)
+		index = append(index, entry.Uint32ToBytes(offset)...)
+		index = append(index, 10)
+	}
+
+	return index
+}
+
+// funkcija koja kreira Summary na osnovu indexTuples, vraća niz bajtova
+func (sstm *SSTableManager) CreateNONMergeSummary(indexTuples []IndexTuple) []byte {
+	summary := make([]byte, 0)
+
+	// čitamo kolika je proredjenost (e.g. 5 znači svaki 5. IndexTuple uzimamo)
+	thinning := config.ReadSummaryThinning()
+
+	// za početak prodjemo kroz listu ključeva i vidimo koji ključ je najmanji i koji je najveći, to treba upisati na početku summary-a
+	keys := make([]string, 0)
+	for _, it := range indexTuples {
+		keys = append(keys, string(it.Key))
+	}
+
+	for i := 0; i < len(keys); i++ {
+		for j := i + 1; j < len(keys); j++ {
+			if keys[i] > keys[j] {
+				keys[i], keys[j] = keys[j], keys[i]
+			}
+		}
+	}
+
+	// upisujemo najmanji i najveći ključ
+	summary = append(summary, []byte(keys[0])...)
+	summary = append(summary, 0)
+	summary = append(summary, []byte(keys[len(keys)-1])...)
+	summary = append(summary, 10)
+
+	// prolazimo kroz listu indexTuples i upisujemo ključeve i pozicije u bloku
+	for i := 0; i < len(indexTuples); i += int(thinning) {
+		summary = append(summary, []byte(indexTuples[i].Key)...)
+		summary = append(summary, 0)
+		summary = append(summary, entry.Uint32ToBytes(indexTuples[i].PositionInBlock)...)
+		summary = append(summary, 10)
+	}
+
+	return summary
 }
