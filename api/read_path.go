6e15bd9bf0d0b1a4c6ef6012fec8e4fec8669d74
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-02-17 07:56:48 +0100

    [ADD] Nastavak ReadPath NONMerge - lociranje unutar index strukture (pronalaženje donjeg i gornjeg offseta prema summary za blokovsku pretragu)

--- api/read_path.go
+++ api/read_path.go
@@ -8,6 +8,7 @@ import (
 	"NASP-NoSQL-Engine/internal/probabilistics"
 	"NASP-NoSQL-Engine/internal/sstable"
 	"encoding/binary"
+	"fmt"
 )
 
 /*
@@ -32,9 +33,13 @@ func NewReadPath(blockManager *block_manager.BlockManager, memtableManager *memt
 
 // ============ SUMMARY I POTREBNE STRUKTURE ============
 type Summary struct {
-	MinKey           []byte // mora ovako da ostane jer ne znamo kako izgleda ključ, da li je bilo kompresije ili ne
-	MaxKey           []byte
-	KeysOffsets      []KeyOffset
+	MinKey []byte // mora ovako da ostane jer ne znamo kako izgleda ključ, da li je bilo kompresije ili ne
+	MaxKey []byte
+
+	MinKeyVarint uint32
+	MaxKeyVarint uint32
+
+	KeysOffsets        []KeyOffset
 	KeysOffsetsVarints []KeyOffsetVarint
 }
 
@@ -51,6 +56,9 @@ type KeyOffsetVarint struct {
 // ======================================================
 
 func (rpo *ReadPath) ReadEntry(key string) (entry.Entry, bool) {
+
+	rpo.BlockManager.ReadBidirectionalMapFromFile() // priprema bidirekcione mape za rad
+
 	// za početak proveravamo da li entry postoji u memtable-u
 	result, exists := rpo.MemtableManager.Find(key)
 	if exists {
@@ -89,8 +97,26 @@ func (rpo *ReadPath) ReadEntry(key string) (entry.Entry, bool) {
 				// učitavamo summary
 				summary := rpo.FindAndDeserializeNONMergeSummary(sstable.SSTableName, sstable.BlockSize, compression)
 
-				// printujemo kako izgleda summary
+				// printujemo kako izgleda summary ------------------------>>>>>> obrisati kasnije
 				summary.Print(compression)
+
+				// proveravamo da li se ključ nalazi u opsegu summarija (radimo sa string ili byte verzijom ključa)
+				stringLowerBound, stringUpperBound := rpo.SetBounds(key, summary, compression)
+
+				if key < stringLowerBound || key > stringUpperBound {
+					continue // ključ nije u opsegu summarija, idemo na sledeću sstabelu
+				} else {
+					// funkcija koja koriguje Bounds za pretragu
+					stringLowerBound = rpo.CorrectLowerBound(key, summary, compression)
+					stringUpperBound = rpo.CorrectUpperBound(key, summary, compression)
+					fmt.Println("Lower bound: ", stringLowerBound)
+					fmt.Println("Upper bound: ", stringUpperBound)
+				}
+
+				// sada idemo u index strukturu i tražimo ključ
+				found, offset := rpo.FindInIndex(sstable.SSTableName, sstable.BlockSize, key, stringLowerBound, stringUpperBound, summary, compression)
+
+				// nastaviće se... Bogdan
 
 			}
 		}
@@ -99,20 +125,128 @@ func (rpo *ReadPath) ReadEntry(key string) (entry.Entry, bool) {
 	return entry.Entry{}, false
 }
 
+// funkcija koja pronalazi ključ u index strukturi
+func (rpo *ReadPath) FindInIndex(sstableName string, blockSize uint32, key string, stringLowerBound string, stringUpperBound string, summary *Summary, compression bool) (bool, uint32) {
+	// sada prema lower bound, upper bound i summary-ju, odredimo donji i gornji offset za pretragu u indexu
+	lowerOffset, upperOffset := rpo.SetOffsetsForIndexSearch(stringLowerBound, stringUpperBound, summary, compression)
+
+	// sada proveravamo da li se ključ nalazi u opsegu lower i upper offseta u indexu
+
+	// Nastaviće se... Bogdan
+
+	return false, 0
+}
+
+// funkcija koja postavlja lower i upper offset za pretragu u indexu
+func (rpo *ReadPath) SetOffsetsForIndexSearch(stringLowerBound string, stringUpperBound string, summary *Summary, compression bool) (uint32, uint32) {
+	lowerOffset := uint32(0)
+	upperOffset := uint32(0)
+
+	if compression {
+		for _, keyOffset := range summary.KeysOffsetsVarints { // tehnički uvek je varint, samo su ovde ključevi u varint formatu (za kompresiju)
+			if rpo.BlockManager.BidirectionalMap.GetByUint32(keyOffset.Key) == stringLowerBound {
+				lowerOffset = keyOffset.Offset
+			}
+			if rpo.BlockManager.BidirectionalMap.GetByUint32(keyOffset.Key) == stringUpperBound {
+				upperOffset = keyOffset.Offset
+			}
+		}
+	} else {
+		for _, keyOffset := range summary.KeysOffsets {
+			if string(keyOffset.Key) == stringLowerBound {
+				lowerOffset = keyOffset.Offset
+			}
+			if string(keyOffset.Key) == stringUpperBound {
+				upperOffset = keyOffset.Offset
+			}
+		}
+	}
+
+	/*
+		jako bitno naglasiti da upper offset mora biti veći
+		od lower offseta, jer se u indexu ključevi nalaze u rastućem redosledu
+		a ovde se može desiti (jako često) da upper offset ostane nula
+		o čemu moramo voditi računa
+		odnosno da će se koristiti upper offset samo u slučaju da je veći od lower offseta
+
+		KADA JE UPPER OFFSET NULA, RADIMO PRETRAGU DO KRAJA FAJLA
+	*/
+
+	return lowerOffset, upperOffset
+}
+
+// funkcija koja postavlja lower i upper bound za pretragu
+func (rpo *ReadPath) SetBounds(key string, summary *Summary, compression bool) (string, string) {
+
+	stringLowerBound := ""
+	stringUpperBound := ""
+	if compression {
+		stringLowerBound = rpo.BlockManager.BidirectionalMap.GetByUint32(summary.MinKeyVarint)
+		stringUpperBound = rpo.BlockManager.BidirectionalMap.GetByUint32(summary.MaxKeyVarint)
+	} else {
+		stringLowerBound = string(summary.MinKey)
+		stringUpperBound = string(summary.MaxKey)
+	}
+
+	return stringLowerBound, stringUpperBound
+}
+
+// funkcija koja koriguje upper bound za pretragu
+func (rpo *ReadPath) CorrectUpperBound(key string, summary *Summary, compression bool) string {
+	// jednostavno iteriramo kroz summarij i na koji naidjemo da je ključ < od njega, to je novi upper bound
+	// idemo od kraja summarija
+	stringUpperBound := ""
+	if compression {
+		for i := len(summary.KeysOffsetsVarints) - 1; i >= 0; i-- {
+			if key < rpo.BlockManager.BidirectionalMap.GetByUint32(summary.KeysOffsetsVarints[i].Key) {
+				stringUpperBound = rpo.BlockManager.BidirectionalMap.GetByUint32(summary.KeysOffsetsVarints[i].Key)
+			}
+		}
+	} else {
+		for i := len(summary.KeysOffsets) - 1; i >= 0; i-- {
+			if key < string(summary.KeysOffsets[i].Key) {
+				stringUpperBound = string(summary.KeysOffsets[i].Key)
+			}
+		}
+	}
+
+	return stringUpperBound
+}
+
+// funkcija koja koriguje lower bound za pretragu
+func (rpo *ReadPath) CorrectLowerBound(key string, summary *Summary, compression bool) string {
+	// jednostavno iteriramo kroz summarij i na koji naidjemo da je ključ >= od njega, to je novi lower bound
+	stringLowerBound := ""
+	if compression {
+		for _, keyOffset := range summary.KeysOffsetsVarints {
+			if key >= rpo.BlockManager.BidirectionalMap.GetByUint32(keyOffset.Key) {
+				stringLowerBound = rpo.BlockManager.BidirectionalMap.GetByUint32(keyOffset.Key)
+			}
+		}
+	} else {
+		for _, keyOffset := range summary.KeysOffsets {
+			if key >= string(keyOffset.Key) {
+				stringLowerBound = string(keyOffset.Key)
+			}
+		}
+	}
+
+	return stringLowerBound
+}
+
 // funkcija koja printuje summary
 func (summary *Summary) Print(compression bool) {
-	// printujemo min i max ključ
-	println("Min key: ", summary.MinKey)
-	println("Max key: ", summary.MaxKey)
 
 	// printujemo sve ključeve i offsete
 	if compression {
+		println("MinKey: ", summary.MinKeyVarint, " MaxKey: ", summary.MaxKeyVarint)
 		for _, keyOffset := range summary.KeysOffsetsVarints {
 			println("Key: ", keyOffset.Key, " Offset: ", keyOffset.Offset)
 		}
 	} else {
+		println("MinKey: ", string(summary.MinKey), " MaxKey: ", string(summary.MaxKey))
 		for _, keyOffset := range summary.KeysOffsets {
-			println("Key: ", keyOffset.Key, " Offset: ", keyOffset.Offset)
+			println("Key: ", string(keyOffset.Key), " Offset: ", keyOffset.Offset)
 		}
 	}
 }
@@ -177,15 +311,25 @@ func DeserializeSummary(summaryBytes []byte, compression bool) (*Summary, error)
 
 	if compression { // odnosno ako smo pisali umesto ključa njegovu varint reprezentaciju
 		// prvo pročitamo min i max ključ
-		minKey := encoded_entry.ReadVarint(summaryBytes)
-		summary.MinKey = minKey
-		// onda imamo 0 bajt
-		summaryBytes = summaryBytes[len(minKey)+1:]
+		minKeyBytes := encoded_entry.ReadVarint(summaryBytes)
+		minKeyVarint, err := encoded_entry.VarintToUint32(minKeyBytes)
+		if err != nil {
+			return nil, err
+		}
+		summary.MinKeyVarint = minKeyVarint
 
-		maxKey := encoded_entry.ReadVarint(summaryBytes)
-		summary.MaxKey = maxKey
+		// onda imamo 0 bajt
+		summaryBytes = summaryBytes[len(minKeyBytes)+1:]
+
+		maxKeyBytes := encoded_entry.ReadVarint(summaryBytes)
+		maxKeyVarint, err := encoded_entry.VarintToUint32(maxKeyBytes)
+		if err != nil {
+			return nil, err
+		}
+		summary.MaxKeyVarint = maxKeyVarint
+
 		// onda imamo 10 bajt (newline)
-		summaryBytes = summaryBytes[len(maxKey)+1:]
+		summaryBytes = summaryBytes[len(maxKeyBytes)+1:]
 
 		// iteriramo kroz summary bajtove (SVAKI RED JE TIPA ključ, 0, offset, 10)
 		for len(summaryBytes) > 0 {
